2472dcc147e825971793a0283317cacfdf80db95
ad-fleming <adfleming@gmail.com>
2020-10-22 10:49:08 -0400

    tested out minifying the controller code and sticking it in a dist file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,53 +1,53 @@
-const express = require ("express");
-const router = express.Router();
+// const express = require ("express");
+// const router = express.Router();
 
-// Import the model (burger.js) to use its database functions
-const burger = require("../models/burger.js")
+// // Import the model (burger.js) to use its database functions
+// const burger = require("../models/burger.js")
 
-// ROUTES GO HERE
+// // ROUTES GO HERE
 
-// Get (READ/VIEW ROUTE)
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
-  });
+// // Get (READ/VIEW ROUTE)
+// router.get("/", function(req, res) {
+//     burger.all(function(data) {
+//       var hbsObject = {
+//         burgers: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+//   });
 
-// POST (CREATE ROUTE)
-// router.post("/api/burgers")
-router.post("/api/burgers", function(req, res) {
-    burger.create("burger_name",
-      req.body.burger_name, 
-      function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
+// // POST (CREATE ROUTE)
+// // router.post("/api/burgers")
+// router.post("/api/burgers", function(req, res) {
+//     burger.create("burger_name",
+//       req.body.burger_name, 
+//       function(result) {
+//       // Send back the ID of the new quote
+//       res.json({ id: result.insertId });
+//     });
+//   });
 
 
 
-// PUT (UPDATE ROUTE)
-// router.put("/api/burgers/:id")
-router.put("/api/burgers/:id", function(req, res) {
-    var condition = req.params.id;
+// // PUT (UPDATE ROUTE)
+// // router.put("/api/burgers/:id")
+// router.put("/api/burgers/:id", function(req, res) {
+//     var condition = req.params.id;
   
-    console.log("condition", condition);
+//     console.log("condition", condition);
   
-    burger.update('devoured', condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
-  });
+//     burger.update('devoured', condition, function(result) {
+//       if (result.changedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
+//   });
   
 
 
-//Export for use in server.js
-module.exports = router;
+// //Export for use in server.js
+// module.exports = router;
