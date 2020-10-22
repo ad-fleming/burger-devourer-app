36d71e53b6549bd54caa9089e2a2b50a2e155a68
ad-fleming <adfleming@gmail.com>
2020-10-22 10:53:41 -0400

    switched back to see if it works again on heroku

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-// var routes = require("./controllers/burgers_controller.js");
-var routes = require("./dist/index.js")
+var routes = require("./controllers/burgers_controller.js");
+// var routes = require("./dist/index.js")
 
 app.use(routes);
 
